9541e92c7643335720141c24df686f4cf7e30aa4
gytks4 <gytks4@naver.com>
2021-11-10 00:17:38 +0900

    dev: 파일 세팅

---
+++ template-js-cra-eslint/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const App = () => {
+  return <StyledRoot></StyledRoot>;
+};
+
+export default App;
+
+const StyledRoot = styled.div``;
